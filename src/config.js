67b74898faf945d81819ea90ddb942156cd2d41c
Andreas � <andreas.wolf@diva-e.com>
2017-11-27 16:04:48 +0100

    � Emoji added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  author: 'Andreas Wolf',
+  author: 'Andreas 🐺',
   title: 'Bezier Abstraction',
   instructions: 'First you choose a Source Image. <br/> <br/> This image will then get triangulated and drawen as a Background (no Magic). <br/><br/> After this first triangulation a second triangulation will be performed, but with slightly changed parameters. The Algorithm of drawing the Lines inside these Triangles, is the same as the Algorithm which is used to draw <a href="https://en.wikipedia.org/wiki/B%C3%A9zier_curve#Constructing_B.C3.A9zier_curves" target="_blank">Quadratic Bezier Curves</a>. <br/>This Mesh of Lines atop of the triangulation gives the Artwork the feeling of depth and detail. <br/><br/> Bonus:<br/> You can upload your Image into a Imgur Album',
 };
